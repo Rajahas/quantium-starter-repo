09e2bd300c3b0050d197bc43abd959d087be94d8
Rajahas <rajahas2006@gmail.com>
2026-04-07 18:28:46 +0100

    task5 testing

---
+++ codeTesting/testing.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import app
+from dash import html, dcc
+
+def find_component(component, component_type):
+    if isinstance(component, component_type):
+        return True
+    if hasattr(component, "children"):
+        children = component.children
+        if isinstance(children, list):
+            return any(find_component(child, component_type) for child in children)
+        else:
+            return find_component(children, component_type)
+    return False
+
+def test_header_present():
+    layout = app.app.layout
+    assert find_component(layout, html.H1)
+
+def test_graph_present():
+    layout = app.app.layout
+    assert find_component(layout, dcc.Graph)
+
+def test_region_picker_present():
+    layout = app.app.layout
+    assert find_component(layout, dcc.RadioItems)
